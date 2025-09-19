c72fe6c5abf1139b23ec696d111a80fec03cbea8
MeHaSa1 <hakansavas564@gmail.com>
2025-09-19 10:12:40 +0300

    commit

--- src/main/java/tr/com/turksat/FeatureWriter.java
+++ src/main/java/tr/com/turksat/FeatureWriter.java
@@ -4,49 +4,81 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class FeatureWriter {
+    private static int valueNumber = 1;
     public static void main(String[] args) {
         StringBuilder feature = new StringBuilder();
         WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         driver.manage().window().maximize();
-        String url = "https://www.turkiye.gov.tr/kadikoy-belediyesi-arsa-rayic-degeri-sorgulama-v2";
+        boolean login = true;
+        if(login){
+            driver.get("https://giris.turkiye.gov.tr/Giris/gir");
+            driver.findElement(By.id("tridField")).sendKeys("tc");
+            driver.findElement(By.id("egpField")).sendKeys("şifre");
+            driver.findElement(By.name("submitButton")).click();
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("searchField")));
+        }
+        String url = "https://www.turkiye.gov.tr/yok-ogrenci-belgesi-sorgulama";
         driver.get(url);
         String serviceName = driver.findElement(By.xpath("//*[@id=\"pageContentBlock\"]/section[1]/div[1]/h2/em")).getText();
         feature.append("Feature: ").append(serviceName).append("\n");
         WebElement content = driver.findElement(By.id("contentStart"));
         String serviceDesc = content.findElement(By.className("richText")).getText();
-        WebElement form = content.findElement(By.id("mainForm"));
-        ArrayList<String> inputNames = new ArrayList<>();
-        for(WebElement input : form.findElements(By.xpath(".//select | .//input[@type= 'text']"))) {
-            inputNames.add(input.findElement(By.xpath("./parent::*")).findElement(By.xpath("./*")).getText());
-        }
-        inputNames.removeIf(s -> s.trim().isEmpty());
-        int value = 0;
-        if(inputNames.isEmpty()){
-            feature.append("\tScenario: ").append(serviceDesc).append("\n");
-            feature.append("\t\tGiven user is on the page \"").append(url).append("\"\n");
-            feature.append("\t\tWhen user presses button");
-        }
-        else{
-            feature.append("\tScenario Outline: ").append(serviceDesc).append("\n");
-            feature.append("\t\tGiven user is on the page \"").append(url).append("\"\n");
-            feature.append("\t\tWhen user enters \"<value1>\" to ").append(inputNames.getFirst()).append("\n");
-            value++;
-            for(String name : inputNames.subList(1, inputNames.size())){
-                value++;
-                feature.append("\t\tAnd user enters \"<value").append(value).append(">\" to ").append(name).append("\n");
+        ArrayList<Input> inputs = new ArrayList<>();
+        List<String> buttonTypes = Arrays.asList("button","submit");
+        List<String> textTypes = Arrays.asList("text","password");
+        int valueCount = 0;
+        for(WebElement input : content.findElements(By.xpath(".//select | .//input | .//a"))) {
+            if(!input.isDisplayed()) continue;
+            if(input.getTagName().equals("select")){
+                inputs.add(new Input("select",getLabel(input)));
+                valueCount++;
+                continue;
             }
-            feature.append("\t\tAnd user presses button\n");
+            if(input.getTagName().equals("a")){
+                inputs.add(new Input("link",input.getText()));
+                continue;
+            }
+            if(input.getTagName().equals("input")){
+                String type = input.getAttribute("type");
+                if(buttonTypes.contains(type)){
+                    inputs.add(new Input("button",input.getAttribute("value")));
+                    continue;
+                }
+                if(textTypes.contains(type)){
+                    inputs.add(new Input("text",getLabel(input)));
+                    valueCount++;
+                }
+            }
+        }
+        if(valueCount == 0){
+            feature.append("\tScenario: ").append(serviceDesc).append("\n");
+        }
+        else {
+            feature.append("\tScenario Outline: ").append(serviceDesc).append("\n");
+        }
+        feature.append("\t\tGiven user is on the page \"").append(url).append("\"\n");
+        if(!inputs.isEmpty()){
+            feature.append("\t\tWhen ").append(toStepText(inputs.getFirst())).append("\n");
+        }
+        for (Input input : inputs.subList(1, inputs.size())) {
+            feature.append("\t\tAnd ").append(toStepText(input)).append("\n");
         }
         feature.append("\t\tThen user should be directed to next page\n");
-        if(value > 0){
+        if(valueCount > 0){
             feature.append("\t\tExamples:\n\t\t\t| ");
-            for (int i = 1 ; i <= value ; i++){
+            for (int i = 1 ; i <= valueCount ; i++){
                 feature.append("value").append(i).append(" | ");
             }
             feature.append("\n");
@@ -57,5 +89,29 @@ public class FeatureWriter {
             System.out.println("File error");
         }
         driver.quit();
+    }
+
+    private static String getLabel(WebElement element){
+        return element.findElement(By.xpath("./parent::*")).findElement(By.xpath("./*")).getText();
+    }
+
+    private static String toStepText(Input input){
+        String stepText;
+        if(input.type.equals("text") || input.type.equals("select")){
+            stepText = "user enters \"<value" + valueNumber + ">\" to " + input.name + " " + input.type;
+            valueNumber++;
+            return stepText;
+        }
+        stepText = "user clicks the " + input.name + " " + input.type;
+        return stepText;
+    }
+}
+
+class Input{
+    String type;
+    String name;
+    Input(String type, String name){
+        this.type = type;
+        this.name = name;
     }
 }
--- test.feature
+++ test.feature
@@ -1,10 +1,5 @@
-Feature: Arsa Metrekare Birim Değeri Sorgulama
-	Scenario Outline: Bu hizmeti kullanarak belediye sınırları içerisindeki cadde ve sokaklara ait arsa rayiç değerini (metrekare birim değeri) yıllara göre sorgulayabilirsiniz.
-		Given user is on the page "https://www.turkiye.gov.tr/kadikoy-belediyesi-arsa-rayic-degeri-sorgulama-v2"
-		When user enters "<value1>" to *Mahalle Adı
-		And user enters "<value2>" to Cadde/Sokak Adı
-		And user enters "<value3>" to *Yıl
-		And user presses button
+Feature: 
+	Scenario: Bu hizmeti kullanarak, Öğrenci bilgilerinizi görüntüleyebilir ve barkodlu Öğrenci belgenizi oluşturabilirsiniz.
+		Given user is on the page "https://www.turkiye.gov.tr/yok-ogrenci-belgesi-sorgulama"
+		When user clicks the Belge Oluştur link
 		Then user should be directed to next page
-		Examples:
-			| value1 | value2 | value3 | 
